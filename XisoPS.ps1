7248de07c1cd2406e1617d7bcedcaf63a9f31325
Haxman <jarrodlives@hotmail.com>
2022-06-18 20:01:02 +1000

    Add Powershell Script version 1

---
+++ XisoPS.ps1
@@ -0,0 +1,22 @@
+﻿Set-Location (Split-Path $MyInvocation.MyCommand.Path)
+
+Get-ChildItem * -Include *.zip -Recurse |Where-Object { $_.Name} |ForEach-Object { 
+
+$zname = (Get-Item $_.Name).BaseName
+
+$zname2 = ".iso" 
+$zname3 = $zname+$zname2
+$zname4 = ".zip"
+$zname7 = $zname+$zname4
+$zname5 = """"+$zname3+""""
+$zname6 = """"+$zname+""""
+
+Expand-Archive -LiteralPath $_.Name -DestinationPath ".\"
+Remove-Item -Path .\$zname7
+Invoke-Expression ".\extract-xiso.exe $zname5"
+Remove-Item -Path .\$zname3
+Invoke-Expression ".\extract-xiso.exe -c $zname6"
+Remove-Item -Path .\$zname -Force -Recurse
+
+
+}
